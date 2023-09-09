e621dfa5ed615c8411a28ef6c6e032c31034e3a3
Jamil Mokadi <70823828+JamilMokadi@users.noreply.github.com>
2023-09-09 14:10:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+streamlit.header("Fruityvice Fruit Advice!")
 
 #new Section to display fruityvice api response
 import requests
